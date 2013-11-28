c7cc4c9ad4c64b983d5b50460a0463cb5a338c0e
aakashparikh <aakash.parikh@samssara.com>
2013-11-28 10:44:16 +0530

    my fact code

---
+++ fact/facto.java
@@ -0,0 +1,27 @@
+//Note: Scanner class work with JDK1.5 or above
+import java.util.*;
+class Prime
+{
+	public static void main(String args[])
+	{		
+		int n, i, res;
+		boolean flag=true;
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Please Enter a No.");
+		n=scan.nextInt();
+		for(i=2;i<=n/2;i++)
+		{
+			res=n%i;
+			if(res==0)
+			{
+				flag=false;
+				break;
+			}
+		}
+		if(flag)
+			System.out.println(n + " is Prime Number");
+		else
+			System.out.println(n + " is not Prime Number");
+	}
+}
+    
